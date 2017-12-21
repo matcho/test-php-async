fd8906ac1fd6c5df408331e256bafdc082295cd3
Mathias Chouet <mathias.chouet@cirad.fr>
2017-12-21 13:43:25 +0100

    Début de test avec amphp

--- amphp.php
+++ amphp.php
@@ -1,4 +1,41 @@
+<?php
+	include "./vendor/autoload.php";
+	include "./config.php";
+?>
+
 <h1>Test Amp</h1>
 <a href="https://amphp.org/">https://amphp.org/</a>
+<br><br>
 
 <?php
+putenv('AMP_PHP_BINARY=/usr/bin/php'); // compatibilité PHP-FPM; marche pas :/
+$apb = getenv('AMP_PHP_BINARY');
+echo "AMP_PHP_BINARY: [$apb]<br>";
+
+use Amp\Promise;
+use function Amp\ParallelFunctions\parallelMap;
+
+$resultats = [];
+
+$tempsAvant = time();
+
+
+$values = Promise\wait(parallelMap(
+		[[1, 3], [2, 4], [3, 3], [4, 1]],
+		function ($conf) use ($testServiceUrl, $tempsAvant, &$resultats) {
+
+			$url = $testServiceUrl . '?ord=' . $conf[0] . '&att=' . $conf[1];
+			$res = file_get_contents($url);
+			$tempsChelou = time();
+			$duree = $tempsChelou - $tempsAvant;
+			echo "J'écris des trucs dans le corbaque (temps passé: $duree sec)<br>";
+			array_push($resultats, $res);
+}));
+
+
+$tempsApres= time();
+$dureeA = $tempsApres - $tempsAvant;
+echo "J'écris des machins après le wait (temps passé: $dureeA sec.)<br>";
+
+// y a tout qu'est bon :)
+echo "<pre>"; var_dump($resultats); echo "</pre>";
--- icicle.php
+++ icicle.php
@@ -1,4 +1,45 @@
-<h1>Test icicle</h1>
+<?php
+	include "./vendor/autoload.php";
+	include "./config.php";
+?>
+
+<h1>Test icicle (deprecated)</h1>
 <a href="https://icicle.io/">https://icicle.io/</a>
+<br><br>
 
 <?php
+use Icicle\Awaitable;
+use Icicle\Coroutine\Coroutine;
+use Icicle\Loop;
+
+$generator = function () {
+    try {
+        // Sets $start to the value returned by microtime() after approx. 1 second.
+        $start = (yield Awaitable\resolve(microtime(true))->delay(1));
+
+        echo "Sleep time: ", microtime(true) - $start, "<br>";
+
+        // Throws the exception from the rejected promise into the coroutine.
+        yield Awaitable\reject(new Exception('Rejected promise<br>'));
+    } catch (Exception $e) { // Catches promise rejection reason.
+        echo "Caught exception: ", $e->getMessage(), "\n";
+    }
+
+    yield Awaitable\resolve('Coroutine completed<br>');
+};
+
+$coroutine = new Coroutine($generator());
+
+$coroutine->done(function ($data) {
+    echo $data, "\n";
+});
+
+echo "Coucou les amis<br>";
+
+Loop\run();
+
+echo "Il faut beau non ?<br>";
+
+//Loop\stop();
+
+echo "Ça roule ?<br>";
